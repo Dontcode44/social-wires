e37727bb228409dd6e1c3da4001a3ecbf0a2d849
Dontcode44 <kabutto378@gmail.com>
2022-11-30 13:16:26 -0500

    [ADD] - CreateMessage service and controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Userd } from 'src/auth/decorators/token.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { MiddleGuard } from 'src/auth/guards/middle.guard';
+import { User } from 'src/users/entities/user.entity';
+import { CreateMessageDto } from './dto/create-message.dto';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
+
+  @UseGuards(MiddleGuard)
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async createMessage(
+    @Userd() user: User,
+    @Body() createMessageDto: CreateMessageDto,
+  ) {
+    return await this.messagesService.createMessage(user.id, createMessageDto);
+  }
 }
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersService } from 'src/users/users.service';
+import { Repository } from 'typeorm';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { Message } from './entities/message.entity';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  constructor(
+    @InjectRepository(Message)
+    private readonly messagesRepository: Repository<Message>,
+    private readonly usersService: UsersService,
+  ) {}
+
+  /**
+   * We're creating a message and saving it to the database
+   * @param {string} userId - The id of the user who is sending the message.
+   * @param {CreateMessageDto} createMessageDto - This is the DTO that we created earlier.
+   * @returns The message that was created.
+   */
+  async createMessage(userId: string, createMessageDto: CreateMessageDto) {
+    const userFound = await this.usersService.findById(userId);
+
+    const message = this.messagesRepository.create({
+      ...createMessageDto,
+    });
+    message.user = userFound;
+    await this.messagesRepository.save(message);
+    return message;
+  }
+}
